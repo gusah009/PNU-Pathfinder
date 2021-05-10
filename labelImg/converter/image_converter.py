395fa2952b0ad17a5af02cbd750064bad9e8f400
JaewookLee <ghimmk@naver.com>
2021-05-10 21:36:03 +0900

    기계관 추가 이미지 xml 업데이트 및 image_converter 수정

--- labelImg/converter/image_converter.py
+++ labelImg/converter/image_converter.py
@@ -8,28 +8,46 @@ def formatNumber(num):
 
 
 if __name__ == "__main__":
-    # path = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/sampleIMG/"
 
-    path = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/sampleIMG_2/"
+    startNum = 841
+
+    # path = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/sampleIMG/
+    # path = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/sampleIMG_2/"
+    path = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/sampleIMG_3/"
+
     newPath = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/sample_jpg/"
 
-    for fileName in os.listdir(path):
+    # path = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/testimg/" # for test
+    # newPath = "/mnt/c/shasuri/assignment/3g_1s/sw_arch_exp/sample_images/testimg/testjpg/" # for test
+
+    for fileSeq, fileName in enumerate(os.listdir(path)):
 
         # s = "img (99).png"
+        # s = "158_0_14369088.8105600,4195296.4592317.png"
+
+        # print("seq : " + str(fileName))
+        # print("name : " + fileSeq)
+
+        '''
         if fileName[:3] == "img":
             fileInfo = fileName.split("(")[1].split(").")
 
             fileNum = fileInfo[0]
-            fileExt = fileInfo[1]
+        '''
 
-            newExt = "jpg"
-            # newName = path + formatNumber(fileNum) + "." + fileExt
-            newName = newPath + formatNumber(fileNum) + "." + newExt
+        fileNum = str(startNum + fileSeq)
 
-            # when just change file name
-            # os.rename(path + fileName, newName)
+        fileExt = "png"
+        newExt = "jpg"
 
-            # when convert image extension, don't use upper line with this one
+        # newName = path + formatNumber(fileNum) + "." + fileExt
+        newName = newPath + formatNumber(fileNum) + "." + newExt
+
+        # when just change file name
+        # os.rename(path + fileName, newName)
+
+        # when convert image extension, don't use upper line with this one
+        if fileName[-4:] == '.' + fileExt:
             im = Image.open(path + fileName)
             im = im.convert("RGB")
             im.save(newName)
