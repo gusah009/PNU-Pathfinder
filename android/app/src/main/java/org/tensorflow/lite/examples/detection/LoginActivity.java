ac5b24e8e9c87ef52ba878f576f2ee72432bef5e
YumYumNyang <khw121699@gmail.com>
2021-05-23 15:53:40 +0900

    크롤러 완료.

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/LoginActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/LoginActivity.java
@@ -17,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -46,30 +48,152 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void run(){
                     try {
-                        System.out.println( login_id.toString());
-                        System.out.println( login_pw.toString());
-                        Map<String, String> data = new HashMap<>();
-                        data.put("id", login_id.toString());
-                        data.put("pw", login_pw.toString());
 
-                        Connection.Response res = Jsoup.connect("https://sso.pusan.ac.kr/LoginServlet?method=idpwProcessEx&ssid=49")
-                                .data(data)
+
+                        Map<String, String> data = new HashMap<>();
+                        data.put("userid", login_id.toString());
+                        data.put("password", login_pw.toString());
+                        data.put("redirect", "/");
+
+                        // 로그인(POST)
+                        Connection.Response response = Jsoup.connect("https://everytime.kr/user/login")
+                                .userAgent("Mozilla/5.0")
+                                .timeout(3000)
+                                .header("Origin", "https://everytime.kr")
+                                .header("Referer", "https://everytime.kr/")
+                                .header("Host", "api.everytime.kr")
+                                .header("Sec-Fetch-Dest", "document")
+                                .header("sec-ch-ua-mobile", "?0")
+                                .header("Sec-Fetch-Mode", "navigate")
+                                .header("Sec-Fetch-Site", "same-origin")
+                                .header("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"")
+                                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                                .header("Content-Type", "application/x-www-form-urlencoded")
+                                .header("Accept-Encoding", "gzip, deflate, br")
+                                .header("Accept-Language", "ko-KR,ko;q=0.9,en;q=0.8")
+                                .header("Sec-Fetch-User", "?1")
+                                .header("Upgrade-Insecure-Requests", "1")
+                                .data("userid",login_id.getText().toString())
+                                .data("password",login_pw.getText().toString())
+                                .data("redirect","/")
                                 .method(Connection.Method.POST)
                                 .execute();
 
-                        Document doc = res.parse();
-                        Map<String, String> cookies = res.cookies();
+                        Map<String, String> loginCookie = response.cookies();
+                        String cookie = response.cookie("etsid");
 
-                        Document doc2 = Jsoup.connect("https://e-onestop.pusan.ac.kr/index?home=home")
-                                .cookies(cookies)
-                                .get();
-                        System.out.println(doc2.html());
-                        System.out.println(cookies);
+                        System.out.println(cookie);
 
-//                        Document doc2 = Jsoup.connect("https://e-onestop.pusan.ac.kr/middleware/study/privateTimeTable/getPrivateRegistrationYear")
-//                                .cookie(cookies)
+
+                        Connection.Response response2 = Jsoup.connect("https://api.everytime.kr/find/timetable/table/list/semester")
+                                .header("Accept", "*/*")
+                                .header("Accept-Encoding", "gzip, deflate, br")
+                                .header("Accept-Language", "ko-KR,ko;q=0.9,en;q=0.8")
+                                .header("Connection", "keep-alive")
+                                .header("Content-Length", "20")
+                                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36")
+                                .header("Origin", "https://everytime.kr")
+                                .header("Referer", "https://everytime.kr/")
+                                .header("Host", "api.everytime.kr")
+                                .header("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"")
+                                .header("sec-ch-ua-mobile", "?0")
+                                .header("Sec-Fetch-Dest", "empty")
+                                .header("Sec-Fetch-Mode", "cors")
+                                .header("Sec-Fetch-Site", "same-site")
+                                .data("year","2021")
+                                .data("semester","1")
+                                .timeout(10000)
+                                .cookie("etsid",cookie)
+                                .method(Connection.Method.POST)
+                                .execute();
+
+
+//                        System.out.println(response2.parse());
+
+                        String id_for_std = response2.parse().selectFirst("table").id();
+                        System.out.println(id_for_std);
+
+                        Connection.Response response3 = Jsoup.connect("https://api.everytime.kr/find/timetable/table")
+                                .header("Accept", "*/*")
+                                .header("Accept-Encoding", "gzip, deflate, br")
+                                .header("Accept-Language", "ko-KR,ko;q=0.9,en;q=0.8")
+                                .header("Connection", "keep-alive")
+                                .header("Content-Length", "11")
+                                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36")
+                                .header("Origin", "https://everytime.kr")
+                                .header("Referer", "https://everytime.kr/")
+                                .header("Host", "api.everytime.kr")
+                                .header("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"")
+                                .header("sec-ch-ua-mobile", "?0")
+                                .header("Sec-Fetch-Dest", "empty")
+                                .header("Sec-Fetch-Mode", "cors")
+                                .header("Sec-Fetch-Site", "same-site")
+                                .data("id",id_for_std)
+                                .timeout(10000)
+                                .cookie("etsid",cookie)
+                                .method(Connection.Method.POST)
+                                .execute();
+
+                        System.out.println(response3.parse().select("table"));
+
+
+//                        Map<String, String> data2 = new HashMap<>();
+//                        data2.put("id", "******");
+//                        data2.put("limit_num", "20");
+//                        data2.put("start_num", "0");
+//                        data2.put("moiminfo", "true");
+//
+//                        Connection.Response response2 = Jsoup.connect("https://api.everytime.kr/find/board/article/list")
+//                                .userAgent("Mozilla/5.0")
+//                                .timeout(3000)
+//                                .header("Origin", "https://everytime.kr")
+//                                .header("Referer", "https://everytime.kr/")
+//                                .header("Accept", "*/*")
+//                                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+//                                .header("Accept-Encoding", "gzip, deflate, br")
+//                                .header("Accept-Language", "ko-KR,ko;q=0.9,en;q=0.8")
+//                                .header("Connection", "keep-alive")
+//                                .header("Content-Length", "48")
+//                                .header("Host", "api.everytime.kr")
+//                                .header("sec-ch-ua", "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"")
+//                                .header("sec-ch-ua-mobile", "?0")
+//                                .header("Sec-Fetch-Dest", "empty")
+//                                .header("Sec-Fetch-Mode", "cors")
+//                                .header("Sec-Fetch-Site", "same-site")
+//                                .data(data2)
+//                                .cookies(loginCookie)
+//                                .method(Connection.Method.POST)
+//                                .execute();
+//
+//                        System.out.println(respon);
+//                        Elements infos = response2.select("");
+//                        System.out.println(infos);
+//                        System.out.println( login_id.toString());
+//                        System.out.println( login_pw.toString());
+//                        Map<String, String> data = new HashMap<>();
+//                        data.put("id", login_id.toString());
+//                        data.put("pw", login_pw.toString());
+//
+//                        Connection.Response res = Jsoup.connect("https://sso.pusan.ac.kr/LoginServlet?method=idpwProcessEx&ssid=49")
+//                                .data(data)
+//                                .method(Connection.Method.POST)
+//                                .execute();
+//
+//                        Document doc = res.parse();
+//                        Map<String, String> cookies = res.cookies();
+//
+//                        Document doc2 = Jsoup.connect("https://e-onestop.pusan.ac.kr/index?home=home")
+//                                .cookies(cookies)
 //                                .get();
+//                        System.out.println(doc2.html());
 //                        System.out.println(cookies);
+//
+////                        Document doc2 = Jsoup.connect("https://e-onestop.pusan.ac.kr/middleware/study/privateTimeTable/getPrivateRegistrationYear")
+////                                .cookie(cookies)
+////                                .get();
+////                        System.out.println(cookies);
                     }
                     catch (IOException e){
                         e.printStackTrace();
